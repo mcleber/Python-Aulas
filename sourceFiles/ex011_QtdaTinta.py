9a1f6fda472d2a7102cf0debc994cc0ea36a83fc
Cleber Moretti <100326566+mcleber@users.noreply.github.com>
2022-04-01 21:54:29 -0300

    Update ex011_QtdaTinta.py

--- sourceFiles/ex011_QtdaTinta.py
+++ sourceFiles/ex011_QtdaTinta.py
@@ -1,4 +1,3 @@
-#desafio 011
 #Faça um programa que leia a largura e a altura de uma parede em metros, calcule a sua área e a quantidade
 #de tinta necessária para pintá-lo, sabendo que cada litro de tinta, pinta uma área de 2m quadrados.
 
