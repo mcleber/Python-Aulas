30aeffc60153780aca9dc73298232b5917e51783
Cleber Moretti <100326566+mcleber@users.noreply.github.com>
2022-04-01 21:52:57 -0300

    Update ex008_ConverterCmMm.py

--- sourceFiles/ex008_ConverterCmMm.py
+++ sourceFiles/ex008_ConverterCmMm.py
@@ -1,5 +1,13 @@
-#desafio 008
 #Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros.
+
+'''
+mm = medida * 1000 milimetro
+cm = medida * 100 centimetro
+dm = medida * 10  decimetro
+dam = medida / 10 decametro
+hm = medida / 100 hectometro
+km = medida / 1000 kilometro
+'''
 
 medida = float(input("Digite um valor em metros: "))
 
@@ -7,10 +15,3 @@ print('O valor em metros é {:.2f}m, '
       '\no valor em centímetros é {:.2f}cm e \n'
       'o valor em milímetros é {:.2f}mm'
       .format(medida, (medida*100), (medida*1000)))
-
-# mm = medida * 1000 milimetro
-# cm = medida * 100 centimetro
-# dm = medida * 10  decimetro
-# dam = medida / 10 decametro
-# hm = medida / 100 hectometro
-# km = medida / 1000 kilometro
