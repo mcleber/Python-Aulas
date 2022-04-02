feffad40732e99a4bdf7370628afb2bec2bd45e8
Cleber Moretti <100326566+mcleber@users.noreply.github.com>
2022-04-01 22:05:35 -0300

    Update ex032_AnoBissexto.py

--- sourceFiles/ex032_AnoBissexto.py
+++ sourceFiles/ex032_AnoBissexto.py
@@ -9,7 +9,7 @@ from datetime import date
 
 ano = int(input('Que ano quer analisar? Coloque 0 para analisar o ano atual: '))
 if ano ==0:
-    ano = date.today().year # Vê a data de hoje e pega só o ano
+    ano = date.today().year # pega a data do PC
 
 if ano % 4 == 0 and ano % 100 != 0 or ano % 400 == 0:
     print('O ano {} é BISSEXTO'.format(ano))
