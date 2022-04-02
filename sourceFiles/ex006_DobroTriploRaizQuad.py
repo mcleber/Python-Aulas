4a184c0639ab3fa4317f65b3ce771d41492e0a4a
Cleber Moretti <100326566+mcleber@users.noreply.github.com>
2022-04-01 21:51:47 -0300

    Update ex006_DobroTriploRaizQuad.py

--- sourceFiles/ex006_DobroTriploRaizQuad.py
+++ sourceFiles/ex006_DobroTriploRaizQuad.py
@@ -1,4 +1,3 @@
-#desafio 006
 #Crie um algoritmo que leia um número e mostre o seu dobro, triplo e raiz quadrada.
 
 n = int(input('Digite um número: '))
@@ -9,7 +8,3 @@ print('O número digitado é {}. \nO dobro é {}. '
       '\nO triplo é {}. '
       '\nA raiz quadrada é {:.2f}.'
       .format(n, (n*2), (n*3), pow(n, (1/2))))
-
-
-# exponenciacao de 1/2, ou seja, X ** (1/2) é a raiz quadrada
-#print('O número digitado é {}. \nO dobro é {}. \nO triplo é {}. \nA raiz quadrada é {:.2f}.'.format(n, (n*2), (n*3), (n**(1/2))))
